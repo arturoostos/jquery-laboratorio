6294b3a325e772fd583eccdd1ac6b4686b5ef54e
arturo ostos <arturoostos@hotmail>
2013-10-26 19:46:43 -0500

    carpeta jquery-laboratorio

---
+++ jquery-lab.js
@@ -0,0 +1,25 @@
+$(document).ready(funtion(){
+
+//elementos ocultos iniciales
+$('#BorrarDesorden').hide();
+$('#aprobarToggle').hide();
+$('#probarToggle').hide();
+//--------------------------------------
+
+// aplicando estilo CSS desde javascript
+$('h1').css({
+color: '#454657',
+font-size: '88px',
+});
+
+//aplicando clase 'colores' del documento 'estilos-laboratorio.css'
+$('section').addClass('.colores');
+});
+
+// aplicando clase animacion ' +
+$('#animaciones').addClass('animacion');
+});
+
+$('#quitarClase').click(function(){
+	$('section').removeClass('colores')
+{);
